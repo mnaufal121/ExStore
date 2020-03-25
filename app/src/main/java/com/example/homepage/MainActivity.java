729993c051c0c60a5b3ff67279d9777329113355
mnaufal121 <naufalalghifari121@gmail.com>
2020-03-25 18:46:14 +0700

    first commit

---
+++ app/src/main/java/com/example/homepage/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.homepage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView daftar = (TextView)findViewById(R.id.register);
+        daftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, MenuRegister.class);
+                startActivity(i);
+            }
+        });
+
+
+    }
+}
