c4bb3ab29669174f576baabb07ae0d09261a9651
Muhammad Naufal Al Ghifari <naufalalghifari121@gmail.com>
2020-05-08 13:24:44 +0700

    Menu Dashboard
    Menu Setelah masuk ke aplikasi

--- app/src/main/java/com/example/homepage/MainActivity.java
+++ app/src/main/java/com/example/homepage/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,13 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//ini text view biar jelas loh
 
         TextView daftar = (TextView)findViewById(R.id.register);
         daftar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this, MenuRegister.class);
+                startActivity(i);
+            }
+        });
+
+        Button masuk = (Button) findViewById(R.id.signin);
+        masuk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, MenuDashboard.class);
                 startActivity(i);
             }
         });
--- app/src/main/java/com/example/homepage/MenuRegister.java
+++ app/src/main/java/com/example/homepage/MenuRegister.java
@@ -2,7 +2,11 @@ package com.example.homepage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MenuRegister extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MenuRegister extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_register);
+
+
+        Button daftar = (Button) findViewById(R.id.daftar);
+        daftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MenuRegister.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
