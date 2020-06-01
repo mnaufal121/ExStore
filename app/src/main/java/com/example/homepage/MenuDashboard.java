5142d282b838d56082484aba49b655b8e6782b6f
Wahyu Priyo Wicaksono <49259298+wpwwahyu@users.noreply.github.com>
2020-06-01 15:17:37 +0700

    Menu Setting
    Pembuatan halaman menu setting

--- app/src/main/java/com/example/homepage/MenuDashboard.java
+++ app/src/main/java/com/example/homepage/MenuDashboard.java
@@ -2,7 +2,11 @@ package com.example.homepage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MenuDashboard extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MenuDashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_dashboard);
+
+        ImageButton settingbtn = (ImageButton) findViewById(R.id.settingbtn);
+        settingbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MenuDashboard.this, MenuSetting.class);
+                startActivity(i);
+            }
+        });
     }
 }
