de2d53f12a063ccc1b6394d705bdb300d63aac71
Wahyu Priyo Wicaksono <49259298+wpwwahyu@users.noreply.github.com>
2020-06-03 16:49:06 +0700

    Popup menu
    Tambahan popup menu pada button bahasa di setting

--- app/src/main/java/com/example/homepage/MenuSetting.java
+++ app/src/main/java/com/example/homepage/MenuSetting.java
@@ -2,18 +2,34 @@ package com.example.homepage;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MenuSetting extends AppCompatActivity {
+public class MenuSetting extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_setting);
-
+        Button showBahasa = findViewById(R.id.bahasa);
         Button logout = (Button) findViewById(R.id.logout);
+
+        showBahasa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Membuat Instance/Objek dari PopupMenu
+                PopupMenu popupMenu = new PopupMenu(MenuSetting.this, view);
+                popupMenu.setOnMenuItemClickListener(MenuSetting.this);
+                popupMenu.inflate(R.menu.menu_bahasa);
+                popupMenu.show();
+            }
+        });
+
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -21,5 +37,18 @@ public class MenuSetting extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.menu1:
+                Toast.makeText(getApplicationContext(),"Anda Memilih Bahas Indonesia",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu2:
+                Toast.makeText(getApplicationContext(),"You choose English",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
     }
 }
