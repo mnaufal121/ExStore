1ba4f5b9276d3c1a6692a13468d07a6dd97459ec
Rafterbang <49393459+Rafterbang@users.noreply.github.com>
2020-06-12 17:04:31 +0700

    Penambahan menu barang 2

---
+++ app/src/main/java/com/example/homepage/MenuBarang2.java
@@ -0,0 +1,24 @@
+package com.example.homepage;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuBarang2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_barang2);
+    }
+
+    public void btn_chat(View view) {
+        Toast.makeText(getApplicationContext(), "Mohon maaf, fitur ini masih hanya hiasan. Terima Kasih:v.", Toast.LENGTH_LONG).show();
+    }
+
+    public void btn_beli(View view) {
+        Toast.makeText(getApplicationContext(), "Mohon maaf, fitur ini masih hanya hiasan. Terima Kasih:v.", Toast.LENGTH_LONG).show();
+    }
+}
